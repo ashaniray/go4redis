9db6d8cda71e3ad34106b26511d59ec959d0d23f
Anirban Gupta <agupta666@gmail.com>
2015-09-09 00:24:54 +0530

    added DEL and EXISTS commands

--- utils.go
+++ utils.go
@@ -1,38 +1,47 @@
 package go4redis
 
 import (
-  "errors"
-  "strconv"
+	"errors"
+	"strconv"
 )
 
-func ifaceToStringFmt (anything interface{}) (string, error) {
-  switch anything.(type) {
-      case string:
-          return ifaceToString(anything)
-      default:
-        val, err := ifaceToInteger(anything)
-        if err != nil {
-          return "", err
-        }
-        str := strconv.Itoa(val)
-        return str, nil
+func ifaceToStringFmt(anything interface{}) (string, error) {
+	switch anything.(type) {
+	case string:
+		return ifaceToString(anything)
+	default:
+		val, err := ifaceToInteger(anything)
+		if err != nil {
+			return "", err
 		}
+		str := strconv.Itoa(val)
+		return str, nil
+	}
 }
 
-func ifaceToInteger (iface interface{}) (int, error) {
-  val, ok := iface.(int)
-	if (ok == false) {
+func ifaceToInteger(iface interface{}) (int, error) {
+	val, ok := iface.(int)
+	if ok == false {
 		return 0, errors.New("Cannot convert response to interger")
 	} else {
-    return val, nil
-  }
+		return val, nil
+	}
 }
 
-func ifaceToString (iface interface{}) (string, error) {
-  val, ok := iface.(string)
-	if (ok == false) {
+func ifaceToString(iface interface{}) (string, error) {
+	val, ok := iface.(string)
+	if ok == false {
 		return EMPTY_STRING, errors.New("Cannot convert response to string")
 	} else {
-    return val, nil
-  }
+		return val, nil
+	}
+}
+
+func stringsToIfaces(xs []string) []interface{} {
+	var args []interface{}
+
+	for _, v := range xs {
+		args = append(args, v)
+	}
+	return args
 }
