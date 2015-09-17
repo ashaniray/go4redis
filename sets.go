ad909c9f14daec527a841c4b1141e5707037f09a
Ashani Ray <ashaniray@gmail.com>
2015-09-17 12:28:30 +0530

    Initial verion of sets added

--- sets.go
+++ sets.go
@@ -1,32 +1,143 @@
 package go4redis
 
-// SADD key member [member ...]
-// Add one or more members to a set
-// SCARD key
-// Get the number of members in a set
-// SDIFF key [key ...]
-// Subtract multiple sets
-// SDIFFSTORE destination key [key ...]
-// Subtract multiple sets and store the resulting set in a key
-// SINTER key [key ...]
-// Intersect multiple sets
-// SINTERSTORE destination key [key ...]
-// Intersect multiple sets and store the resulting set in a key
-// SISMEMBER key member
-// Determine if a given value is a member of a set
-// SMEMBERS key
-// Get all the members in a set
-// SMOVE source destination member
-// Move a member from one set to another
-// SPOP key [count]
-// Remove and return one or multiple random members from a set
-// SRANDMEMBER key [count]
-// Get one or multiple random members from a set
-// SREM key member [member ...]
-// Remove one or more members from a set
-// SUNION key [key ...]
-// Add multiple sets
-// SUNIONSTORE destination key [key ...]
-// Add multiple sets and store the resulting set in a key
-// SSCAN key cursor [MATCH pattern] [COUNT count]
-// Incrementally iterate Set elements
+
+func (c *Client) SAdd(key string, member ...string) (int, error) {
+  args := append([]string{key}, member...)
+  val, err := c.sendRequest("SADD", stringsToIfaces(args)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SCard(key string) (int, error) {
+  val, err := c.sendRequest("SCARD", key);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SDiff(key ...string) (int, error) {
+  val, err := c.sendRequest("SDIFF", stringsToIfaces(key)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SDIFFSTORE(destination string, key ...string) (int, error) {
+  args := append([]string{destination}, key...)
+  val, err := c.sendRequest("SDIFFSTORE", stringsToIfaces(args)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SINTER(key ...string) (int, error) {
+  val, err := c.sendRequest("SINTER", stringsToIfaces(key)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SINTERSTORE(destination string, key ...string) (int, error) {
+  args := append([]string{destination}, key...)
+  val, err := c.sendRequest("SINTERSTORE", stringsToIfaces(args)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SISMEMBER(key string, member string) (int, error) {
+  val, err := c.sendRequest("SISMEMBER", key, member);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SMEMBERS(key string) (int, error) {
+  val, err := c.sendRequest("SMEMBERS", key);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SMOVE(source string, destination string, member string) (int, error) {
+  val, err := c.sendRequest("SMOVE", source, destination, member);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SPOP(key string, count string) (int, error) {
+  val, err := c.sendRequest("SPOP", key, count);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SRANDMEMBER(key string, count string) (int, error) {
+  val, err := c.sendRequest("SRANDMEMBER", key, count);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SREM(key string, member ...string) (int, error) {
+  args := append([]string{key}, member...)
+  val, err := c.sendRequest("SREM", stringsToIfaces(args)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SUNION(key ...string) (int, error) {
+  val, err := c.sendRequest("SUNION", stringsToIfaces(key)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+
+func (c *Client) SUNIONSTORE(destination string, key ...string) (int, error) {
+  args := append([]string{destination}, key...)
+  val, err := c.sendRequest("SUNIONSTORE", stringsToIfaces(args)...);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+/*
+func (c *Client) SSCAN(key string, cursor string, MATCH string, pattern string, COUNT string, count string) {
+  val, err := c.sendRequest("SSCAN", key, cursor, [MATCH, pattern], [COUNT, count]);
+   if err != nil {
+    return -1, err
+  }
+  i, err := ifaceToInteger(val)
+  return i, err
+}
+*/
